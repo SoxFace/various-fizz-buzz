27562c9732d6786502bd71aef426c1df48bb54e8
soxface <sonyacorcoran@gmail.com>
2023-10-10 13:39:18 +1030

    complete fizzbuzz in node

--- 1-node/src/fizz-buzz.js
+++ 1-node/src/fizz-buzz.js
@@ -7,5 +7,21 @@
  * }]
  */
 module.exports = (start, end) => {
-  throw 'TODO: Implement FizzBuzz'
-}
+  const results = [];
+
+  for (let i = start; i <= end; i++) {
+    let output = '';
+
+    if (i % 3 === 0) {
+      output += 'Fizz';
+    }
+
+    if (i % 5 === 0) {
+      output += 'Buzz';
+    }
+
+    results.push({ input: i, output: output });
+  }
+
+  return results;
+};
