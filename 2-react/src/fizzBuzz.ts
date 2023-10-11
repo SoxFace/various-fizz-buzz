f3d92ae64d71148b685f4562e1c0c3dca6f66e85
soxface <sonyacorcoran@gmail.com>
2023-10-11 13:32:59 +1030

    complete 2-react fizzbuzz

--- 2-react/src/fizzBuzz.ts
+++ 2-react/src/fizzBuzz.ts
@@ -6,5 +6,24 @@ export type FizzBuzzResult = {
 }
 
 export const fizzBuzz = (start: number, end: number): FizzBuzzResult[] => {
-  throw 'TODO: Implement FizzBuzz'
-}
+  const results: FizzBuzzResult[] = [];
+
+  for (let i = start; i <= end; i++) {
+    let output: FizzBuzzOutput = '';
+
+    if (i % 3 === 0) {
+      output += 'Fizz';
+    }
+
+    if (i % 5 === 0) {
+      output += 'Buzz';
+    }
+
+    results.push({
+      input: i,
+      output: output as FizzBuzzOutput,
+    });
+  }
+
+  return results;
+};
