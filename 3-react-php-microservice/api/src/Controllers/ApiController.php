9aeeb73fe13c5f9e520605be06e0a251aca5daf1
soxface <sonyacorcoran@gmail.com>
2023-10-12 12:02:54 +1030

    update the ApiController to use the FizzBuzz class

--- 3-react-php-microservice/api/src/Controllers/ApiController.php
+++ 3-react-php-microservice/api/src/Controllers/ApiController.php
@@ -17,7 +17,19 @@ class ApiController
     }
 
     public function handle(): JsonResponse
-    {
-        // TODO
-    }
+{
+    // Get request parameters (start and end) from query string
+    $start = $_GET['start'] ?? 1;
+    $end = $_GET['end'] ?? 15; // Default end value to 15 to match test suite
+
+    // validate values
+    $start = (int) $start;
+    $end = (int) $end;
+
+    // Calculate result using the FizzBuzz class
+    $result = $this->fizzBuzz->calculate($start, $end);
+
+    // Return the result as JSON
+    return new JsonResponse($result);
+}
 }
