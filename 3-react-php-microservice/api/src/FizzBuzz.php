ff3442deca18b41a62111c88eabb77741d79f7e0
soxface <sonyacorcoran@gmail.com>
2023-10-12 11:59:35 +1030

    complete logic for fizzbuzz

--- 3-react-php-microservice/api/src/FizzBuzz.php
+++ 3-react-php-microservice/api/src/FizzBuzz.php
@@ -9,6 +9,26 @@ class FizzBuzz
 {
     public function calculate(int $start, int $end): array
     {
-        throw new LogicException('TODO: Implement FizzBuzz');
+        $results = [];
+
+        for ($i = $start; $i <= $end; $i++) {
+            $output = '';
+
+            if ($i % 3 === 0) {
+                $output .= 'Fizz';
+            }
+
+            if ($i % 5 === 0) {
+                $output .= 'Buzz';
+            }
+
+            if ($output === '') {
+                $output = '';
+            }
+
+            $results[] = ['input' => $i, 'output' => $output];
+        }
+
+        return $results;
     }
 }
