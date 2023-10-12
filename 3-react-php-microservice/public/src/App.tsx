9ee12d681a51153d458a8f2a3bf01bc7d88d3bf1
soxface <sonyacorcoran@gmail.com>
2023-10-12 12:03:57 +1030

    call and render api data

--- 3-react-php-microservice/public/src/App.tsx
+++ 3-react-php-microservice/public/src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+interface FizzBuzzData {
+  input: number;
+  output: string;
+}
+
 function App() {
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
+  const [data, setData] = useState<FizzBuzzData[]>([]);
+
+  const calculateFizzBuzz = () => {
+    // validate values
+    const startValue = parseInt(start);
+    const endValue = parseInt(end);
+
+    if (!isNaN(startValue) && !isNaN(endValue)) {
+      if (startValue > endValue) {
+        alert('Start value should be less than end value.');
+      }
+      // Fetch FizzBuzz data from api using params
+      axios.get(`http://localhost:8000/?start=${startValue}&end=${endValue}`)
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    } else  {
+      alert('Please enter valid numbers for start and end.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FizzBuzz Data</h1>
+      <div className="input">
+        <div className="input-group">
+          <div className="input-label">Start:</div>
+          <input
+            className="input-field"
+            type="number"
+            value={start}
+            onChange={(e) => setStart(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <div className="input-label">End:</div>
+          <input
+            className="input-field"
+            type="number"
+            value={end}
+            onChange={(e) => setEnd(e.target.value)}
+          />
+          <button className="blue-button" onClick={calculateFizzBuzz}>Calculate</button>
+        </div>
+      </div>
+      <div className="divider"></div> {/* Divider line here */}
+      <div className='fizzbuzz-list'>
+        <ol>
+          {Array.isArray(data) ? (
+            data.map((item, index) => (
+              <li key={index}>{`${item.output || '-'}`}</li>
+            ))
+          ) : (
+            <li>No data available</li>
+          )}
+        </ol>
+      </div>
     </div>
   );
+  
 }
 
 export default App;
